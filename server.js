d92f204311bea20c8611c4e88cc9db2fb670fc42
ScottMillerWesCoas <scott.t.miller1@gmail.com>
2016-11-04 16:21:10 -0700

    routes for funstats, realstats

--- server.js
+++ server.js
@@ -7,41 +7,42 @@ var userController = require('./userController');
 var scraper = require('./scraper');  
 var fs = require('fs'); 
 
- app.use(express.static(path.join(__dirname, './'))); 
+app.use(express.static(path.join(__dirname, './'))); 
 
 app.use(bodyParser.urlencoded({extended: true})); 
 app.use(cookieParser()); 
 
+
+//Send user to NBA.com/twitter/LibertyBallers.com scrape page
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, './login.html')); 
+  res.sendFile(path.join(__dirname, './SixersRealStats.html')); 
 });
 
-
-
-
 app.get('/FunSixers', function(req, res){
-    res.sendFile(path.join(__dirname, './SixersFunStats.html')); 
+  res.sendFile(path.join(__dirname, './SixersFunStats.html')); 
 }); 
 
 app.get('/Sixers', function(req, res){
-    res.sendFile(path.join(__dirname, './SixersRealStats.html')); 
+  res.sendFile(path.join(__dirname, './SixersRealStats.html')); 
 }); 
 
 app.get('/RealSixers', function(req, res){
-    res.sendFile(path.join(__dirname, './SixersRealStats.html')); 
+  res.sendFile(path.join(__dirname, './SixersRealStats.html')); 
 });   
 
+
+//Routes when user modifies Sixers dream team from SixersFunStats.html
 app.post('/addPlayer', userController.addPlayer); 
-app.post('/login', userController.verifyUser); 
-app.post('/signup', userController.createUser); 
 app.post('/update', userController.updateData); 
 app.post('/remove', userController.deletePlayer); 
-
 app.get('/data', userController.sendData); 
+
+//Route when user wants to see real-time tweets/pics/stats on SixersRealStats.html
 app.get('/realData', userController.sendRealData); 
 
+
 app.listen(3076, function(){
-    console.log('you listenin on port 3076, obviously...'); 
+    console.log('you listenin\' on port 3076, obviously...'); 
 }); 
 
 module.exports = app; 
