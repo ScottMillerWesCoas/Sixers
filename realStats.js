98f17dc3b0cf47eb71b655638e404f1b28087f63
ScottMillerWesCoas <scott.t.miller1@gmail.com>
2016-11-04 16:19:26 -0700

    makes ajax call to collect scraped data, sorts data into stats, articles, headlines, pics and displays to user

--- realStats.js
+++ realStats.js
@@ -1,35 +1,32 @@
 
- $(document).ready(function(){
-   $('#changeStats').click(function(){
+$(document).ready(function(){
+    //Head to real-time data scrape sixers page 
+  $('#changeStats').click(function(){
     window.location = './SixersFunStats.html'; 
-   }); 
-    $.ajax({
-        url: "/realData", 
-        //dataType: 'json',
-        success: function(result){
-            var dataTable = result;
-            console.log('DATA IN REALTABLE', dataTable); 
-            dataTable.forEach(function(el, i){
-                if (Array.isArray(el)){ 
-                    el.forEach(function(inel){
-                       $('#top').prepend("<img src=" + inel + " height='10%'>");   
-                    });  
-                }
-                else {
-                    $('#outer').prepend("<div class='datacell' id=" + i + "><h2>" + el.name + "</h2><p class='stats'>Position: " + el.position + "</p><p class='stats'>Games Played: " + el.games + "</p><p class='stats'>FG%: " + el.fgPct + "</p><p class='stats'>3Pt%: " + el.threePtPct + "</p><p class='stats'>REBS: " + el.reb + "</p><p class='stats'>AST: " + el.ast + "</p><p class='stats'>STL: " + el.stl + "</p></div>"); 
-                //}
-                }    
+  }); 
+   //AJAX CALL engages Scraper.js via server then userController, which pulls stats, articles and pictures from the web
+  $.ajax({
+    url: "/realData", 
+    success: function(result){
+      var dataTable = result;
+      dataTable.forEach(function(el, i){
+        if (Array.isArray(el)){ 
+          if (el[0][0] === 'h') { //pics are in nested array where each item is a string that starts with 'h' [['h']]
+            el.forEach(function(inel){
+              $('#top').prepend("<img src=" + inel + " height='10%'>");   
+            });  
+          } else { //article links are objects in an array nested in the larger array - [[{}]]
+            el.forEach(function(inel){
+              $('#top2').append("<a target='blank' style='margin-left: 3%' href=" + inel.href + ">" + inel.text +"</a>");   
             }); 
-    
-        }
-}); 
-
-$('#popData').on('click', function(){
-     console.log('get it goin!'); 
-
-}); 
-
-
-
+          }
+        } else { //player info are objects in the larger array - [{}]
+          $('#outer').prepend("<div class='datacell' id=" + i + "><h2>" + el.name + "</h2><p class='stats'>Position: " + el.position + "</p><p class='stats'>Games Played: " + el.games + "</p><p class='stats'>Points: " + el.points + "</p><p class='stats'>PPG: " + el.PPG + "</p><p class='stats'>FG%: " + el.fgPct + "</p><p class='stats'>3Pt%: " + el.threePtPct + "</p><p class='stats'>REBS: " + el.reb + "</p><p class='stats'>AST: " + el.ast + "</p><p class='stats'>STL: " + el.stl + "</p></div>"); 
+        }    
+      }); //close forEach on retrieved 2D array
+    } //close success function
+  }); //close AJAX call
 });
+
+
 
