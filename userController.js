f8ee3e7dd997aaefc29efc987dd6f6adfc5f402b
ScottMillerWesCoas <scott.t.miller1@gmail.com>
2016-11-04 16:22:49 -0700

    controller handling requests for data/requests to mod/delete data

--- userController.js
+++ userController.js
@@ -6,111 +6,76 @@ var path =require('path');
 var scraper = require('./scraper');  
 var fs = require('fs'); 
 
-var dataArr = scraper.getData();
+var dataArr = scraper.getData(); //Initialize scrape immediately to avoid async issues
 
 mongoose.connect('mongodb://localhost/db'); 
 
 var db = mongoose.connection; 
 db.on('error', console.error.bind(console, 'connection error')); 
 db.once('open', function(){
-    console.log('Connected to DB!!!!');
+    // console.log('Successfully connected to DB via mongoose...');
 }); 
 
 var userController = {}; 
 
-
-userController.createUser = function(req,res){
-    var newUser = {}; 
-    newUser.num = req.body.num; 
-    newUser.username = req.body.username; 
-        User.create(newUser, function(err, user){
-            if (err) throw err; 
-            else console.log('user', user.username);    
-        });
-        res.redirect('./SixersFunStats.html');  
-}; 
-
 userController.addPlayer = function(req,res){
-    console.log('inside addPlayer'); 
-    var newPlayer = {}; 
-    newPlayer.name = req.body.name; 
-    newPlayer.ppg = req.body.ppg; 
-    newPlayer.rpg = req.body.rpg; 
-    newPlayer.apg = req.body.apg;
-    newPlayer.pic = req.body.pic;  
-        Player.create(newPlayer, function(err, user){
-            if (err) throw err; 
-            else console.log('user', user);    
-        });
-        res.redirect('./SixersFunStats.html');  
+  //Using bodyparser in server.js, we collect details from user request, add to database with Player schema, then refresh page to show new data
+  var newPlayer = {}; 
+  newPlayer.name = req.body.name; 
+  newPlayer.ppg = req.body.ppg; 
+  newPlayer.rpg = req.body.rpg; 
+  newPlayer.apg = req.body.apg;
+  newPlayer.pic = req.body.pic;  
+  Player.create(newPlayer, function(err, user){
+    if (err) throw err;   
+  });
+  res.redirect('./SixersFunStats.html');  
 }; 
    
-
 userController.sendData = function(req, res){
-         Player.find({}, function(err, players){
-          // console.log('players in DB:', players);    
-            res.send(players); 
-         });
+  //Data collected from MongoDB, sent to funStats.js
+  Player.find({}, function(err, players){
+    // console.log('players in DB:', players);    
+    res.send(players); 
+ });
 }; 
 
 userController.sendRealData  = function(req, res){
-    setTimeout(function(){
-          //console.log('SEND REAL DATA****', dataArr);
-           res.send(dataArr);  
-    }, 500); 
-
+  //Make sure scraper has ample time to collect all data from various sources if user immediately goes to scraper page
+  setTimeout(function(){
+    res.send(dataArr);  
+  }, 200); 
 }; 
+
 userController.updateData = function(req, res){
-    var query = { 'name': req.body.name};
-    var options = {multi: true};  
-    var update={};
-    update[req.body.stat]=req.body.newStat;
-    console.log('update', update); 
-    Player.update(
-        query,
-        {$set:update},
-        function (err,success) {
-            if(err) return handleError(err);
-            console.log(success); 
-}); 
-    // Player.update(query, { $set: { name : req.body.newStat }}, options, function(err, user){
-    //     if (err) console.log('ERROR:', err); 
-    //     console.log('updated player', user); 
-    // }  ); 
-    res.redirect('./Sixers/SixersFunStats.html'); 
+  //search DB for player name entered by user using Player schema, update data in DB based on user input, refresh page to show updated data
+  var query = { 'name': req.body.name};
+  var options = {multi: true};  
+  var update={};
+  update[req.body.stat]=req.body.newStat;
+  Player.update(
+    query,
+    {$set:update},
+    function (err,success) {
+      if(err) return handleError(err);
+      //console.log(success); 
+  }); 
+  res.redirect('./SixersFunStats.html'); 
 }; 
 
 userController.deletePlayer = function(req, res){
-    console.log('got rem', req.body); 
-    Player.remove({name: req.body.delName}, function(err) {
-        if (!err) {
-            console.log('removed', req.body.delName); 
-        }
-        else {
-            console.log("ERR: ", err); 
-        }
-    });
-    res.redirect('./SixersFunStats.html');  
-}; 
-
-userController.verifyUser = function(req, res){
-    console.log('in verifyUser', req.body); 
-    User.findOne({username: req.body.username}, "username num", function(err, user){
-        if (user){
-            console.log('found user!', user); 
-            if (req.body.num === user.num.toString()){
-                console.log('found a match!!!'); 
-                res.redirect('./SixersFunStats.html'); 
-            } else {
-                console.log('no num?'); 
-            }
-
-        }
-        else res.redirect('./welcome.html'); 
-    }); 
+  //search DB for player name entered by user using Player schema, delete player in DB, refresh page to show updated data
+  Player.remove({name: req.body.delName}, function(err) {
+    if (!err) {
+        //console.log('removed', req.body.delName); 
+    }
+    else {
+        console.log("ERR: ", err); 
+    }
+  });
+  res.redirect('./SixersFunStats.html');  
 }; 
 
 
 module.exports = userController; 
-
 
